35c37cb18ecd35bc3201722a3d6c27bfb8789e3d
bilgelm <murat.bilgel@nih.gov>
2017-10-04 17:41:12 -0400

    added more setup opts

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
 from setuptools import setup
 
 setup(name='temporalimage',
-      version='0.1',
+      version='0.1.0',
       description='simple functions for working with temporal 4D imaging data',
       url='http://github.com/bilgelm/temporalimage',
       author='Murat Bilgel',
       author_email='murat.bilgel@nih.gov',
       license='MIT',
       packages=['temporalimage'],
-      zip_safe=False)
+      install_requires=[
+          'nibabel',
+          'pandas'
+          'scipy'
+          'numpy'
+          'math'
+          'os'
+      ],
+      zip_safe=False,
+      test_suite='nose.collector',
+      tests_require=['nose'])
