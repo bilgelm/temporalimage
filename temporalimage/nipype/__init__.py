17b4904e4c7259f83bc18d866325c975936359d3
bilgelm <murat.bilgel@nih.gov>
2018-04-06 15:43:36 -0400

    bug fix

--- temporalimage/nipype/__init__.py
+++ temporalimage/nipype/__init__.py
@@ -1,1 +1,4 @@
-from .wrapper import SplitTimeSeries, DynamicMean
+try:
+    from .wrapper import SplitTimeSeries, DynamicMean
+except ImportError:
+    print('Install temporalimage using nipype option.')
