af64055906ecc11257d6eeafc0bd65f872ef80bf
Murat Bilgel <bilgelm@gmail.com>
2018-06-08 10:03:51 -0400

    changed tmp output dir structure for nipype test

--- tests/test_nipype.py
+++ tests/test_nipype.py
@@ -2,12 +2,14 @@ try:
     import temporalimage
     from .generate_test_data import generate_fake4D
     import os
+    from shutil import rmtree
+    from uuid import uuid4
     import unittest
     import numpy as np
-    from tempfile import mkdtemp
 
     from temporalimage.nipype_wrapper import SplitTimeSeries, DynamicMean
-    from nipype.pipeline.engine import Node
+    from nipype.pipeline.engine import Node, Workflow
+    from nipype.interfaces.utility import IdentityInterface
 
     class TestTemporalImageNipype(unittest.TestCase):
         def setUp(self):
@@ -15,38 +17,70 @@ try:
             timg = temporalimage.load(imgfile, timingfile)
 
             # make a temporary directory in which to save the temporal image files
-            tmpdirname = mkdtemp()
+            self.tmpdirname = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                                           'tests_output_'+uuid4().hex)
 
-            self.imgfilename = os.path.abspath(os.path.join(tmpdirname,'img.nii.gz'))
-            self.csvfilename = os.path.abspath(os.path.join(tmpdirname,'timingData.csv'))
+            if not os.path.isdir(self.tmpdirname):
+                os.makedirs(self.tmpdirname)
+
+            self.imgfilename = os.path.abspath(os.path.join(self.tmpdirname,'img.nii.gz'))
+            self.csvfilename = os.path.abspath(os.path.join(self.tmpdirname,'timingData.csv'))
 
             temporalimage.save(timg, self.imgfilename, self.csvfilename)
 
         def tearDown(self):
-            os.remove(self.imgfilename)
-            os.remove(self.csvfilename)
+            # remove the tests_output directory
+            rmtree(self.tmpdirname)
+
 
         def test_nipype_split(self):
+            infosource = Node(interface=IdentityInterface(fields=['in_file']), name="infosource")
+            infosource.iterables = ('in_file', [self.imgfilename])
+
             # Split the dynamic scan
-            split_time = Node(interface=SplitTimeSeries(timeSeriesImgFile=self.imgfilename,
+            split_time = Node(interface=SplitTimeSeries(#timeSeriesImgFile=self.imgfilename,
                                                         frameTimingCsvFile=self.csvfilename,
                                                         splitTime=10), name="split_time")
-            split_time.run()
+
+            split_time_workflow = Workflow(name="split_time_workflow",
+                                           base_dir=self.tmpdirname)
+            split_time_workflow.connect([
+                                         (infosource, split_time, [('in_file','timeSeriesImgFile')])
+                                        ])
+            split_time_workflow.run()
 
         def test_nipype_dynamic_mean(self):
-            dynamic_mean = Node(interface=DynamicMean(timeSeriesImgFile=self.imgfilename,
+            infosource = Node(interface=IdentityInterface(fields=['in_file']), name="infosource")
+            infosource.iterables = ('in_file', [self.imgfilename])
+
+            dynamic_mean = Node(interface=DynamicMean(#timeSeriesImgFile=self.imgfilename,
                                                       frameTimingCsvFile=self.csvfilename,
                                                       startTime=13, endTime=42),
                                                       name="dynamic_mean")
-            dynamic_mean.run()
 
-        def test_nipype_dynamic_mean(self):
-            dynamic_mean = Node(interface=DynamicMean(timeSeriesImgFile=self.imgfilename,
+            dynamic_mean_workflow = Workflow(name="dynamic_mean_workflow",
+                                             base_dir=self.tmpdirname)
+            dynamic_mean_workflow.connect([
+                                           (infosource, dynamic_mean, [('in_file', 'timeSeriesImgFile')])
+                                          ])
+            dynamic_mean_workflow.run()
+
+        def test_nipype_dynamic_mean2(self):
+            infosource = Node(interface=IdentityInterface(fields=['in_file']), name="infosource")
+            infosource.iterables = ('in_file', [self.imgfilename])
+
+            dynamic_mean = Node(interface=DynamicMean(#timeSeriesImgFile=self.imgfilename,
                                                       frameTimingCsvFile=self.csvfilename,
                                                       startTime=13, endTime=42,
                                                       weights='frameduration'),
                                                       name="dynamic_mean")
-            dynamic_mean.run()
+
+            dynamic_mean_workflow = Workflow(name="dynamic_mean_workflow",
+                                             base_dir=self.tmpdirname)
+            dynamic_mean_workflow.connect([
+                                           (infosource, dynamic_mean, [('in_file', 'timeSeriesImgFile')])
+                                          ])
+            dynamic_mean_workflow.run()
 
 except ImportError:
     print('Cannot perform temporalimage.nipype tests. \
